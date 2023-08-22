ed70054ffeaf24e30b8553254e86d7dd2762d9ce
Jeonneung <82321178+Jeonneung@users.noreply.github.com>
2023-08-22 14:08:25 +0900

    basic layout

---
+++ public/client.js
@@ -0,0 +1,29 @@
+// const uploadForm = document.getElementById('uploadForm');
+// const photoInput = document.getElementById('photoInput');
+// const uploadedImageContainer = document.getElementById('uploadedImageContainer');
+// const uploadedImage = document.getElementById('uploadedImage');
+
+// uploadForm.addEventListener('submit', async (event) => {
+// event.preventDefault();
+
+// const formData = new FormData();
+// formData.append('photo', photoInput.files[0]);
+
+// try {
+//     const response = await fetch('/upload', {
+//     method: 'POST',
+//     body: formData
+//     });
+
+//     if (response.ok) {
+//     const imagePath = await response.text();
+//     uploadedImage.src = imagePath;
+//     uploadedImageContainer.style.display = 'block';
+//     } else {
+//     console.error('Upload failed');
+//     }
+// } catch (error) {
+//     console.error('Error uploading image:', error);
+// }
+// });
+
