1e348e24e725ad48603f5bc31f8824b435236e1d
Jeonneung <82321178+Jeonneung@users.noreply.github.com>
2023-08-23 09:56:52 +0900

    almost done!!!!!

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = 3000;
 
 // ImagePath 변수 선언
 let uploadedImagePath = '';
-// let ImageResult = '';
+// let result = '';
 
 // view engine을 pug로 설정
 // Pug 템플릿 파일이 위치하는 디렉토리 설정
@@ -40,15 +40,25 @@ app.post('/upload', upload.single('photo'), (req, res) => {
     uploadedImagePath = req.file.path.replace('public', ''); // 업로드된 파일의 저장된 경로 가져옴
     // 이미지 경로를 세션에 저장하거나 데이터베이스에 저장 가능
     // 모델에 이미지 프로세싱 돌려서 결과 보고... 그러면 될듯
-
+    // imageResult = APIprocessingFunc()
     res.redirect('/result');
 });
 
 // 이미지 보여주는 페이지
 app.get('/result', (req, res) => {
     // 세션에서 이미지 경로를 가져오거나 데이터베이스에서 가져올 수 있음
-    res.render('result', { imagePath: uploadedImagePath });
+    res.render('result', { imagePath: uploadedImagePath, result: result });
 });
+
+// 분리수거 인증하는 페이지
+app.get('/authentication', (req, res) => {
+    res.render('authentication')
+})
+
+// 인증 완료 표시 페이지
+app.post('/auth', (req, res) => {
+    res.render('auth')
+})
 
 const handleExcept = (req, res) => {
     return res.send('Sorry, we can\'t find page.')
